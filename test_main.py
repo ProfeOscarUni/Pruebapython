f8cbb30fd1935d96330b27c49b9a1cbafc78d6e6
oscar bautista <ingenierobautistaoscar@gmail.com>
2024-05-02 20:32:55 -0500

    ajuste

--- test_main.py
+++ test_main.py
@@ -30,7 +30,7 @@ def test_LoginPass():
     assert loginPass
 
 def test_LoginFail():
-    loginFail = login("Pruebas","1234")
+    loginFail = login("Pruebas","8952")
     assert not loginFail
 
 if __name__ == '__main__':
